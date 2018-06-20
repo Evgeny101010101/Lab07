3ecbb77752bfd3c51be13ef6de2b4f356b48da6b
Evgeny101010101 <34716187+Evgeny101010101@users.noreply.github.com>
2018-06-20 23:54:25 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Lab07
+[![Build Status](https://travis-ci.org/17viu18m/lab07.svg?branch=master)](https://travis-ci.org/17viu18m/lab07)
---
+++ include/print.hpp
@@ -0,0 +1,6 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+
+void print(const std::string& text, std::ostream& out = std::cout);  	# Print1
+void print(const std::string& text, std::ofstream& out);		# Print2	
